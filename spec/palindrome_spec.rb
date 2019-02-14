bf65685a85a8a417871722da21aff8fe01bf9556
ryuto <rryuto5210@gmail.com>
2019-02-14 20:19:28 +0900

    Update palindrome test

--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -3,14 +3,17 @@ require_relative '../src/palindrome'
 
 RSpec.describe Palindrome do
   describe '#is_palindrome?' do
+    subject { Palindrome.new.is_palindrome?(str) }
     context '回文の場合' do
-      it "返り値はtrueであること" do
-        expect(Palindrome.new.is_palindrome?("しんぶんし")).to eq true
+      let(:str) { 'しんぶんし' }
+      it '返り値はtrueであること' do
+        is_expected.to eq true
       end
     end
     context '回文以外の場合' do
-      it "返り値はfalseであること" do
-        expect(Palindrome.new.is_palindrome?("とんこつ")).to eq false
+      let(:str) { 'とんこつ' }
+      it '返り値はfalseであること' do
+        is_expected.to eq false
       end
     end
   end
